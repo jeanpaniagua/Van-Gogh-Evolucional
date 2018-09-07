d2e9ac14a1e3a346be9faff33600af1439d81eeb
Psicops <k.fernandez41@hotmail.com>
2018-09-06 20:57:23 -0500

    Merge con arreglos de Interfaz

--- src/interfaz/interfaz.java
+++ src/interfaz/interfaz.java
@@ -10,12 +10,10 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
-import manejoImagenes.randomImage;
+
 import manejoImagenes.rgbToGrayScale;
 
 /**
